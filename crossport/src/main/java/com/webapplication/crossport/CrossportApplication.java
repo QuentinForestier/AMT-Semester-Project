2f52c2d85f65e7bfe4d6d626bc714ca8e9f24220
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-10-02 12:09:40 +0200

    Adding showcase project

---
+++ crossport/src/main/java/com/webapplication/crossport/CrossportApplication.java
@@ -0,0 +1,13 @@
+package com.webapplication.crossport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossportApplication.class, args);
+    }
+
+}
---
+++ crossport/src/test/java/com/webapplication/crossport/CrossportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapplication.crossport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

b054e2d94b174cf0e8a4b9f8832ce198e40cdf5d
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-11-06 19:15:23 +0100

    mocking

--- mocking/auth-service/startAuthMock.ps1
+++ mocking/auth-service/startAuthMock.ps1
@@ -5,14 +5,14 @@ Write-Output "--- Retrait du conteneur"
 docker rm amt-mock-auth
 
 # Vérification de l'existence de l'image
-$existImage = docker images -q melvynheig/amt-mock-auth
+$existImage = docker images -q crossport/amt-mock-auth
 if($null -eq $existImage)
 {
-    Write-Output "--- Pull image"
-    docker pull melvynheig/amt-mock-auth
+    Write-Output "--- Build image"
+    docker build -t crossport/amt-mock-auth
     
 }
 
 # Démarrage des conteneurs
 Write-Output "--- Demarrage du conteneur"
-docker run -d -p 8081:8081 --name amt-mock-auth melvynheig/amt-mock-auth
+docker run -d -p 8081:8081 --name amt-mock-auth crossport/amt-mock-auth

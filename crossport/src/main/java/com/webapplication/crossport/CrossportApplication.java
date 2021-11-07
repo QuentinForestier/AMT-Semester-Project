20f6c0bdd5510e1db5560f6f72a4d96181b1d7d1
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-11-07 14:28:15 +0100

    Reseting main

--- crossport/src/main/java/com/webapplication/crossport/CrossportApplication.java
+++ crossport/src/main/java/com/webapplication/crossport/CrossportApplication.java
@@ -1,7 +1,5 @@
 package com.webapplication.crossport;
 
-import com.webapplication.crossport.service.AuthService;
-import com.webapplication.crossport.service.RequestType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,8 +9,7 @@ public class CrossportApplication extends SpringBootServletInitializer
 {
 
     public static void main(String[] args){
-        AuthService.getInstance().makeRequest(RequestType.LOGIN, "admin", "admin");
-        //SpringApplication.run(CrossportApplication.class, args);
+        SpringApplication.run(CrossportApplication.class, args);
     }
 
 }

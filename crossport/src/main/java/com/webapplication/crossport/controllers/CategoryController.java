8688ac456a2b91f00a234afc5d22ff7af6ad0cc3
quillasp <saralfddin93@yahoo.fr>
2021-11-12 17:35:56 +0100

    One can create and delete a category

--- crossport/src/main/java/com/webapplication/crossport/controllers/CategoryController.java
+++ crossport/src/main/java/com/webapplication/crossport/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.webapplication.crossport.models.Category;
@@ -19,12 +20,19 @@ public class CategoryController {
     @GetMapping("/categories")
     public String listCategories(Model model) {
         model.addAttribute("listCategories", categoryService.getAllCategories());
+        model.addAttribute("category", new Category());
         return "categories";
     }
 
     @PostMapping("/saveCategory")
     public String saveCategory(@ModelAttribute("category") Category category) {
         categoryService.saveCategory(category);
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/deleteCategory/{id}")
+    public String deleteCategory(@PathVariable(value = "id") Integer id) {
+        this.categoryService.deleteCategory(id);
         return "redirect:/categories";
     }
 

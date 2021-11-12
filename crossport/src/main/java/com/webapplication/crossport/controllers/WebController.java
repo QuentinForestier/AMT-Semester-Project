06ece85a3ae6d7d41faed4d9594c29f04221ae8c
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-11-12 15:05:58 +0100

    cart page

--- crossport/src/main/java/com/webapplication/crossport/controllers/WebController.java
+++ crossport/src/main/java/com/webapplication/crossport/controllers/WebController.java
@@ -2,12 +2,15 @@ package com.webapplication.crossport.controllers;
 
 import com.webapplication.crossport.models.Article;
 import com.webapplication.crossport.models.ArticleRepository;
+import com.webapplication.crossport.models.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -17,22 +20,38 @@ public class WebController {
     private ArticleRepository articleRepository;
 
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="visitor") String name, Model model) {
+    public String greeting(HttpServletRequest request, @RequestParam(name="name", required=false, defaultValue="visitor") String name, Model model) {
         model.addAttribute("name", name);
+
+        List<Integer> cartInfo = Cart.getCartInSession(request);
+        cartInfo.add(1);
+        cartInfo.add(2);
 
         return "index";
     }
 
     @GetMapping("/shop")
-    public String shop(Model model){
+    public String shop(HttpServletRequest request, Model model){
 
         Optional<Article> p = articleRepository.findById(1);
+
+        List<Integer> cartInfo = Cart.getCartInSession(request);
+        for (int i : cartInfo) {
+            System.out.println(i);
+        }
 
         if(p.isPresent()){
             model.addAttribute("product", p.get());
         }
 
         return "shop";
+    }
+
+    @GetMapping("/cart")
+    public String cart(HttpServletRequest request, Model model){
+
+
+        return "cart";
     }
 
 }

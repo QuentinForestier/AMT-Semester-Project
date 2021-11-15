9adc2baf56214a814536e042d1e479662c266b66
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-11-15 11:59:07 +0100

    panier visiteur ajout

--- crossport/src/main/java/com/webapplication/crossport/controllers/CartController.java
+++ crossport/src/main/java/com/webapplication/crossport/controllers/CartController.java
@@ -2,7 +2,6 @@ package com.webapplication.crossport.controllers;
 
 import com.webapplication.crossport.models.Article;
 import com.webapplication.crossport.models.Cart;
-import com.webapplication.crossport.models.repository.ArticleRepository;
 import com.webapplication.crossport.models.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,14 +21,14 @@ public class CartController {
     @Autowired
     private ArticleService articleService;
 
-    @GetMapping("/addArticle{id}")
-    public String addArticle(HttpServletRequest request, @RequestParam(value = "id", required = true) Integer id, Model model) {
+    @GetMapping("/addArticle")
+    public String addArticle(HttpServletRequest request, @RequestParam(value = "id") Integer id,  @RequestParam(value = "quantity", defaultValue = "1") Integer quantity, Model model) {
 
         Cart cart = Cart.getCartInSession(request.getSession());
 
         try {
             Article article = articleService.getArticleById(id);
-            cart.addToCart(1, article);
+            cart.addToCart(quantity, article);
         } catch (RuntimeException e) {
             e.printStackTrace();
         }
@@ -37,6 +36,14 @@ public class CartController {
         //your controller code
         String referer = request.getHeader("Referer");
         return "redirect:"+ referer;
+    }
+
+    @GetMapping("/cart")
+    public String viewCart(HttpServletRequest request, Model model){
+
+        model.addAttribute("cart", Cart.getCartInSession(request.getSession()));
+
+        return "cart";
     }
 
 }

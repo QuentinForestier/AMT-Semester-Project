e4974d241db0b3dd5ac6f55486586bbe4b56ca32
Berney Alec <beral@sevjnet.ch>
2021-11-30 15:44:22 +0100

    normalize article controller ok

--- scripts/DB_Insert_Datas.sql
+++ scripts/DB_Insert_Datas.sql
@@ -1,8 +1,8 @@
 INSERT INTO article(id, description, img_path, in_stock, name, price)
-VALUES (1, 'joli snowboard', 'images/pic5.jpg', true, 'snowboard', 100),
-	(2, 'jolis skis', 'images/pic5.jpg', true, 'skis', 200),
-	(3, 'ski de fond salomon skating modèle coupe du monde', 'images/pic5.jpg', true, 'ski de fond skating salomon', 500),
-	(4, 'ski freestyle Movement pour le big air', 'images/pic5.jpg', true, 'ski freestyle', 400);
+VALUES (1, 'joli snowboard', '~/images/pic5.jpg', true, 'snowboard', 100),
+	(2, 'jolis skis', '~/images/pic5.jpg', true, 'skis', 200),
+	(3, 'ski de fond salomon skating modèle coupe du monde', '~/images/pic5.jpg', true, 'ski de fond skating salomon', 500),
+	(4, 'ski freestyle Movement pour le big air', '~/images/pic5.jpg', true, 'ski freestyle', 400);
 
 INSERT INTO category(id, name)
 VALUES (1, 'ski'),
